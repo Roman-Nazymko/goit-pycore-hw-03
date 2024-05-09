55084681763cb67df2f896ae18064e94f6f00c0f
Roman Nazymko <rnazimko18@gmail.com>
2024-05-09 09:16:58 +0200

    added task_02

--- task_02.py
+++ task_02.py
@@ -1,13 +1,21 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
+    # Check if the input parameters are within valid ranges
     if min < 1 or max > 1000 or quantity > (max - min + 1):
-        return []
+        return []  # Return an empty list if parameters are invalid
     
+    # Generate 'quantity' unique random numbers within the specified range
     random_numbers = random.sample(range(min, max + 1), quantity)
+
+    # Sort the generated numbers in ascending order
     random_numbers.sort()
 
+    # Return the sorted list of random numbers
     return random_numbers
 
+# Generate a lottery ticket with 6 random numbers between 1 and 49
 lottery_numbers = get_numbers_ticket(1, 49, 6)
+
+# Print the generated lottery numbers
 print("Ваші лотерейні числа:", lottery_numbers) 
